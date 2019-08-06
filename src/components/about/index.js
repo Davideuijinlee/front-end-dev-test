a53bd8a2d0a565f2ef2dd66cf555f30bf90e0efa
Davideuijinlee <Davideuijinlee@gmail.com>
2019-08-05 22:02:50 -0700

    about completed lorem ipsum used needs css revision a link does not link anywhere

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './about.scss';
+import loremImg1 from '../../assets/images/lorem_images/img-1.jpg';
 
 export default () => {
     return(
-        <div className="row aboutContainer"></div>
+        <div className="container-fluid aboutContainer">
+            <div className="row col-12">
+                <div className="aboutTextContainer col-lg-6">
+                    <h1 className="aboutTitle offset-lg-3 col-lg-6">Lorem Ipsum
+                    </h1>
+                    <div className="aboutText offset-lg-3 col-lg-6">Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .
+                    </div>
+
+                    <a href="place" className="offset-lg-3 col-lg-6">Lorem Ipsum</a>
+
+                </div>
+                <div className="aboutImgContainer col-lg-6">
+                    <img className="aboutImg" src={loremImg1} alt="two hikers demonstrating excellent teamwork"/>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/heroImg/index.js
+++ src/components/heroImg/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import hero1 from '../../assets/images/hero_image/hero1.jpg';
-import hero2 from '../../assets/images/hero_image/hero2.jpg';
-import hero3 from '../../assets/images/hero_image/hero3.jpg';
+import hero1 from '../../assets/images/hero_images/hero1.jpg';
+import hero2 from '../../assets/images/hero_images/hero2.jpg';
+import hero3 from '../../assets/images/hero_images/hero3.jpg';
 import './heroImg.scss';
 
 export default () => {
     return (
         <div id="heroImgCarousel" className="carousel slide carousel-fade" data-ride="carousel">
-            <div class="carousel-inner">
+            <div className="carousel-inner">
                 <div className="heroImgContainer carousel-item active" data-interval="5000">
                 {/* eslint-disable-next-line */}
                     <img src={hero1} className="heroImg d-block" alt="Nighttime photo of Golden Gate Bridge in San Francisco"  />
