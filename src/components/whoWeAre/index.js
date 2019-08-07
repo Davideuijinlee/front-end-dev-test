5ca71a363901d075ee5581d755e235535d16b2b7
Davideuijinlee <Davideuijinlee@gmail.com>
2019-08-07 11:29:00 -0700

    asdfsa

--- src/components/whoWeAre/index.js
+++ src/components/whoWeAre/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-import App from '../app.js';
+import App from './components/app.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
