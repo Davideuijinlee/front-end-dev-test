28856cb8ec4698bfe897a25c25dc905b9054f1a5
Davideuijinlee <Davideuijinlee@gmail.com>
2019-08-07 19:04:32 -0700

    started carousal

--- src/components/reviews/index.js
+++ src/components/reviews/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import './reviews.scss';
 import LargeReview from './largeReview';
+import ReviewCarousal from './reviewCarousal';
 
 export default () => {
     return (
         <div className="container-fluid reviewsContainer">
             <div className="row">
-                <div className="d-inline-block largeReviewContainer col-lg-6">
-                    <LargeReview/>
-                    {/* <div className="largeReviewTextContainer">
-                        <h1 className="largeReviewText offset-lg-3 col-lg-6">Lorem ipsum
-                    </h1>
-                        <h1 className="largeReviewText offset-lg-3 col-lg-6">dolor sit amet
-                    </h1>
-                    </div>
-                    <div className="starsContainer offset-lg-3 col-lg-6">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                    </div> */}
-                </div>
-                <div className="d-inline-block reviewCarousalContainer col-lg-6">
-                </div>
+                <LargeReview/>
+                <ReviewCarousal/>
             </div>
         </div>
     )
--- src/components/reviews/largeReview.js
+++ src/components/reviews/largeReview.js
@@ -7,51 +7,51 @@ export default props => {
     // Call the hook passing in ref and root margin
     // In this case it would only be considered onScreen if more ...
     // ... than 300px of element is visible.
-    const onScreen = useOnScreen(ref, '-300px');
+    const onScreen = useOnScreen(ref, '-150px');
 
     return (
-        <Fragment>
-                <div className="largeReviewTextContainer">
-                    ) : (
-            <h1 className="largeReviewText offset-lg-3 col-lg-6">Lorem ipsum</h1>
-                    <h1 className="largeReviewText offset-lg-3 col-lg-6">dolor sit amet</h1>
+        <div className="d-inline-block largeReviewContainer col-lg-6">
+            <div className="largeReviewTextContainer">
+                <div className="row">
+                    <h1 className="largeReviewText offset-2 offset-sm-3 offset-md-4 offset-lg-3 col-lg-6">Lorem ipsum</h1>
+                    <h1 className="largeReviewText offset-2 offset-sm-3 offset-md-4 offset-lg-3 col-lg-6">dolor sit amet</h1>
                 </div>
-
-                <div ref={ref} className={`{ onScreen ? '#23cebd' : '#efefef'
-                }starsContainer offset-lg-3 col-lg-6`}>
-                    <div className="">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+            </div>
+            <div ref={ref} className={`stars offset-3 offset-sm-3 offset-md-4 offset-lg-3 col-lg-6`}>
+                {onScreen ? (
+                    <div className="starsContainerNoScroll">
+                        <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
                     </div>
+                ) : (
+                        <div className='starsContainerScroll'>
+                            <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                        </div>
                     )}
+            </div>
         </div>
-        </Fragment>
-    
-        );
-    }
-    
-    
-    // Hook
+    );
+}
+
+
+// Hook
 function useOnScreen(ref, rootMargin = '0px') {
-    // State and setter for storing whether element is visible
     const [isIntersecting, setIntersecting] = useState(false);
-        
+
     useEffect(() => {
         const observer = new IntersectionObserver(
             ([entry]) => {
-                // Update our state when observer callback fires
                 setIntersecting(entry.isIntersecting);
             },
             {
                 rootMargin
             }
-            );
+        );
         if (ref.current) {
-                observer.observe(ref.current);
-            }
+            observer.observe(ref.current);
+        }
         return () => {
-                observer.unobserve(ref.current);
-            };
-        }, []); // Empty array ensures that effect is only run on mount and unmount
-    
-        return isIntersecting;
-    }
+            observer.unobserve(ref.current);
+        };
+    }, []);
+    return isIntersecting;
+}
